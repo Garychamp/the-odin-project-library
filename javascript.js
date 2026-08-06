775c4c4c2c7c2e5453fb7c56c927c39dc32f680e
Garychamp <garyhoare18@gmail.com>
2026-08-06 19:30:07 +0100

    Reorganize code structure by moving variable declarations and adding event listener for book form dialog

--- javascript.js
+++ javascript.js
@@ -1,16 +1,21 @@
-const myLibrary = [];
-
 function Book(title, author, pages, isRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
+  this.id = crypto.randomUUID();
 }
+
+const myLibrary = [];
 
 function addBookToLibrary(title, author, pages, isRead) {
   myLibrary.push(new Book(title, author, pages, isRead));
 }
 
+const bookFormButton = document.getElementById("book-form-button");
+const bookFormDialog = document.getElementById("book-form-dialog");
 
-
+bookFormButton.addEventListener ("click", () => {
+  bookFormDialog.showModal();
+});
 
