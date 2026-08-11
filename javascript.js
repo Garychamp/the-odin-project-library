327ca44f40a80ed0edb89cb4675c20451b2c27ef
Garychamp <garyhoare18@gmail.com>
2026-08-11 13:47:46 +0100

    Refactor render function to create book cards and add remove/toggle read buttons

--- javascript.js
+++ javascript.js
@@ -21,21 +21,23 @@ const libraryContainer = document.getElementById('library');
 function render() {
   libraryContainer.innerHTML = "";
   myLibrary.forEach((book) => {
+    const bookCard = document.createElement ("div");
+    bookCard.classList.add("book-card");
     const titleEl = document.createElement('p');
       titleEl.textContent = book.title;
-      libraryContainer.appendChild(titleEl);
+      bookCard.appendChild(titleEl);
     const authorEl = document.createElement('p');
       authorEl.textContent = book.author;
-      libraryContainer.appendChild(authorEl);
+      bookCard.appendChild(authorEl);
     const pagesEl = document.createElement('p');
       pagesEl.textContent = "Pages: " +book.pages;
-      libraryContainer.appendChild(pagesEl);
+      bookCard.appendChild(pagesEl);
     const isRead = document.createElement('p');
       isRead.textContent = book.isRead ? "Read" : "Not Read";
-      libraryContainer.appendChild(isRead);
+      bookCard.appendChild(isRead);
     const removeBtn = document.createElement("button");
       removeBtn.textContent = "Remove";
-      libraryContainer.appendChild(removeBtn);
+      bookCard.appendChild(removeBtn);
      removeBtn.addEventListener("click", () => {
     const bookId = book.id;
       myLibrary = myLibrary.filter((b) => b.id !== bookId);
@@ -43,14 +45,15 @@ function render() {
 });
     const toggleBtn = document.createElement("button");
     toggleBtn.textContent = "Toggle Read";
-    libraryContainer.appendChild(toggleBtn);
+    bookCard.appendChild(toggleBtn);
     toggleBtn.addEventListener("click", () => {
     const bookId = book.id;
     const foundBook = myLibrary.find((b) => b.id === bookId);
       foundBook.isRead = !foundBook.isRead;
   render();
 });
-})}
+libraryContainer.appendChild(bookCard);
+})};
 
 bookFormButton.addEventListener("click", () => {
   bookFormDialog.showModal();
